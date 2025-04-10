bc510c0f9a6d85299be6e3ea53ec4008a22432ea
Stephon Ricks <nova@moondreams.dev>
2025-04-09 22:39:37 -0400

    add app icons + splash screen

--- app/(core)/_layout.tsx
+++ app/(core)/_layout.tsx
@@ -1,14 +1,8 @@
-import { Text } from 'react-native';
 import { Redirect, Stack } from 'expo-router';
 import { useSession } from '../../contexts/SessionContext';
 
 export default function Layout() {
-  const { session, isLoading } = useSession();
-
-  // You can keep the splash screen open, or render a loading screen like we do here.
-  if (isLoading) {
-    return <Text>Loading...</Text>;
-  }
+  const { session } = useSession();
 
   // Only require authentication within the (app) group's layout as users
   // need to be able to access the (auth) group and sign in again.
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,21 @@
 import { Stack } from 'expo-router';
-import { SessionProvider } from '../contexts/SessionContext';
 import '.././global.css';
+import { SessionProvider, useSession } from '../contexts/SessionContext';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const { isLoading } = useSession();
+
+  useEffect(() => {
+    if (!isLoading) {
+      SplashScreen.hideAsync();
+    }
+  }, [isLoading]);
+
   // Set up the auth context and render our layout inside of it.
   return (
     <SessionProvider>
